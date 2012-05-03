48ea2893d8b235d6cfb1da759414fcef6fc24300
Ben Speakmon <bspeakmon@atlassian.com>
2012-05-02 17:57:37 -0700

    - convert tools to package - fix jirashell installation in setuptools - bump to version 0.6

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='jira-python',
-    version='0.5.0',
+    version='0.6.0',
     packages=find_packages(),
-    scripts=['tools/jirashell'],
 
-    install_requires=['requests==0.11.2', 'simplejson==2.5.0', 'argparse==1.2.1',],
-    extras_require = {
-        'jirashell': ['ipython==0.12.1']
+    install_requires=['requests==0.11.2', 'ipython==0.12.1', 'argparse==1.2.1'],
+#   can't get this working for the moment.
+#    extras_require = {
+#        'interactive-shell': ['ipython==0.12.1', 'argparse==1.2.1']
+#    },
+    entry_points = {
+        'console_scripts':
+            ['jirashell = tools.jirashell:main'],
     },
 
     url='http://bitbucket.org/bspeakmon_atlassian/jira-python',
