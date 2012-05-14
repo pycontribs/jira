36da3c3a9e9d6a103fee9cdefa09e72d71445241
Ben Speakmon <bspeakmon@atlassian.com>
2012-05-14 12:20:50 -0700

    - make python 2.7 requirement explicit

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
