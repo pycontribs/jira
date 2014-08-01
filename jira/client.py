1dc08352a5cad2c51fded7fefe868adf2b626316
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2014-08-01 17:12:40 +0100

    v0.31 merge with bitbcket copy.

--- jira/client.py
+++ jira/client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -26,7 +27,7 @@ import json
 import warnings
 
 from six import string_types
-from six.moves import html_parser
+from six.moves.html_parser import HTMLParser
 from six import print_ as print
 
 
@@ -50,6 +51,8 @@ if 'pydevd' not in sys.modules:
         pass
     except NotImplementedError:
         pass
+
+warnings.simplefilter('default')
 
 #encoding = sys.getdefaultencoding()
 # if encoding != 'UTF8':
@@ -151,6 +154,11 @@ class JIRA(object):
         """
         if options is None:
             options = {}
+            if server and hasattr(server, 'keys'):
+                warnings.warn("Old API usage, use JIRA(url) or JIRA(options={'server': url}, when using dictionary always use named parameters.", DeprecationWarning)
+                options = server
+                server = None
+
         if server:
             options['server'] = server
         if async:
@@ -185,11 +193,6 @@ class JIRA(object):
             self.session()  # This will raise an Exception if you are not allowed to login. It's better to fail faster than later.
         # We need version in order to know what API calls are available or not
         self._version = tuple(self.server_info()['versionNumbers'])
-
-    def __del__(self):
-        # that is performing the logout by killing the session
-        self.async_do()
-        # self.kill_session()
 
     def _check_for_html_error(self, content):
         # TODO: Make it return errors when content is a webpage with errors
@@ -587,6 +590,7 @@ class JIRA(object):
         url = self._get_url('issue')
         r = self._session.post(url, headers={'content-type': 'application/json'}, data=json.dumps(data))
         raise_on_error(r)
+
         raw_issue_json = json.loads(r.text)
         if prefetch:
             return self.issue(raw_issue_json['key'])
@@ -2213,7 +2217,7 @@ class GreenHopper(JIRA):
         """
         Return the completed issues for the given board id and sprint id
         """
-        r_json = self._get_json('rapid/charts/sprintreport?rapidViewId=%s&sprintId=%s' % (board_id, sprint_id), self.GREENHOPPER_BASE_URL)
+        r_json = self._get_json('rapid/charts/sprintreport?rapidViewId=%s&sprintId=%s' % (board_id, sprint_id), base=self.GREENHOPPER_BASE_URL)
         issues = [Issue(self._options, self._session, raw_issues_json) for raw_issues_json in r_json['contents']['incompletedIssues']]
         return issues
 
@@ -2347,3 +2351,13 @@ class GreenHopper(JIRA):
         url = self._get_url('rank', base=self.GREENHOPPER_BASE_URL)
         r = self._session.put(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
         raise_on_error(r)
+
+if __name__ == '__main__':
+    url = 'http://issues.apache.org/jira'
+    print("1")
+    jira = JIRA(url)
+    print("2")
+    jira = JIRA(options={'server': url})
+    print("3 (should see a warning)")
+    j = JIRA({'server': url})
+    print(jira.issue('INFRA-7976'))
