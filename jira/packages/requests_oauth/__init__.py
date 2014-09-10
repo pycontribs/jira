91eeda9dc9e36a22be4ca0ed0a70ee4e7bb6de2f
sorin.sbarnea@gmail.com <sorin.sbarnea@gmail.com>
2014-09-10 12:38:05 +0100

    Fixing #81 by removing requests_oauth from the package.

--- jira/packages/requests_oauth/__init__.py
+++
@@ -1,7 +0,0 @@
-# requests-oauth 0.4.0
-# Hacked to support RSA-SHA1 encryption for Atlassian OAuth.
-# Original author: Miguel Araujo
-# Forked from https://github.com/maraujop/requests_oauth
-# Original license: 3-clause BSD
-
-from hook import OAuthHook
