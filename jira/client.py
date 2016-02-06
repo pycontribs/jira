cd503df62db93f019c69e8e883c7d92e3bef0334
casahome2000 <casahome2000@gmail.com>
2016-02-05 22:41:45 -0800

    resolves issue where incompleted_issues() was failing for missing key; 'incompletedIssues' corrected to 'issuesNotCompletedInCurrentSprint'

--- jira/client.py
+++ jira/client.py
@@ -2774,8 +2774,10 @@ class JIRA(object):
                       DeprecationWarning)
         r_json = self._get_json('rapid/charts/sprintreport?rapidViewId=%s&sprintId=%s' % (board_id, sprint_id),
                                 base=self.AGILE_BASE_URL)
+
+        # r_json does not contain 'incompletedIssues' changed to 'issuesNotCompletedInCurrentSprint'
         issues = [Issue(self._options, self._session, raw_issues_json) for raw_issues_json in
-                  r_json['contents']['incompletedIssues']]
+                  r_json['contents']['issuesNotCompletedInCurrentSprint']]
         return issues
 
     # TODO: remove sprint_info() method, sprint() method suit the convention more
