c5062d42d971b777ff06c8dffab6091f669989e5
Guy Matz <guy.matz.cft@jcrew.com>
2016-11-22 17:27:38 -0500

    added ability to create/query Jira webhooks

--- jira/resources.py
+++ jira/resources.py
@@ -48,7 +48,8 @@ __all__ = (
     'Status',
     'User',
     'CustomFieldOption',
-    'RemoteLink'
+    'RemoteLink',
+    'Webhook'
 )
 
 logging.getLogger('jira').addHandler(NullHandler())
@@ -763,6 +764,31 @@ class Version(Resource):
         """Comparison."""
         return self.id == other.id and self.name == other.name
 
+class Webhook(Resource):
+    """A JIRA webhook."""
+
+    WEBHOOK_BASE_URL = '{server}/rest/webhooks/{agile_rest_api_version}/{path}'
+
+    def __init__(self, options, session, raw=None):
+        Resource.__init__(self, 'webhook/{0}', options, session, self.WEBHOOK_BASE_URL)
+        if raw:
+            self._parse_raw(raw)
+
+    def enable(self):
+        """
+        Enable this webhook on the server.
+        """
+        super(Webhook, self).update(enabled=True)
+
+    def disable(self):
+        """
+        Disable this webhook on the server.
+        """
+        super(Webhook, self).update(enabled=False)
+
+    def __eq__(self, other):
+        return self.self == other.self
+
 
 # GreenHopper
 
@@ -884,6 +910,7 @@ resource_class_map = {
     r'status/[^/]+$': Status,
     r'user\?username.+$': User,
     r'version/[^/]+$': Version,
+    r'webhook/[^/]+$': Webhook,
     # GreenHopper specific resources
     r'sprints/[^/]+$': Sprint,
     r'views/[^/]+$': Board}
