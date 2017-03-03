084768feebf31c8bc769d90726fa6c6b39f6bb19
Igor Pavlenko <ip@leboutique.com>
2017-03-03 14:36:14 +0200

    Remove warning

--- tests/tests.py
+++ tests/tests.py
@@ -354,7 +354,6 @@ class JiraTestManager(object):
                                                    self.project_c_name,
                                                    template_name='IT Service Desk')
                 except Exception:
-                    logging.warning(e)
                     pass
 
                 sleep(1)  # keep it here as often JIRA will report the
