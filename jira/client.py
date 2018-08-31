33f12fd9b395487bbef06fe21c2f7e7eec71c06b
Gerstein, Dan <daniel.gerstein@bamtechmedia.com>
2018-08-31 12:15:40 -0700

    Add documentation line and fix non-default-to None param

--- jira/client.py
+++ jira/client.py
@@ -3061,13 +3061,14 @@ class JIRA(object):
     """
 
     @translate_resource_args
-    def boards(self, startAt=0, maxResults=50, type=None, name=None, projectKeyOrID=projectKeyOrID):
+    def boards(self, startAt=0, maxResults=50, type=None, name=None, projectKeyOrID=None):
         """Get a list of board resources.
 
         :param startAt: The starting index of the returned boards. Base index: 0.
         :param maxResults: The maximum number of boards to return per page. Default: 50
         :param type: Filters results to boards of the specified type. Valid values: scrum, kanban.
         :param name: Filters results to boards that match or partially match the specified name.
+        :param projectKeyOrID: Filters results to boards that match the specified project key or ID.
         :rtype: ResultList[Board]
 
         When old GreenHopper private API is used, paging is not enabled and all parameters are ignored.
