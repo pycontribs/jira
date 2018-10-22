a1b05e12d424bd1ec7139e358c01278ccb1c2db4
merazola <44301314+merazola@users.noreply.github.com>
2018-10-22 09:33:13 -0400

    Removed whitespaces.

--- jira/client.py
+++ jira/client.py
@@ -3754,10 +3754,8 @@ class JIRA(object):
             for p in project_ids.split(','):
                 ids.append(self.project(p).id)
             project_ids = ','.join(ids)
-       
         if location_id is not None:
             location_id = self.project(location_id).id
-
         payload['name'] = name
         if isinstance(project_ids, string_types):
             project_ids = project_ids.split(',')
