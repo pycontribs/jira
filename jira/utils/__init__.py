9caed80a031421182102d64ea0d4645947fd2e64
niculice <niculice@adobe.com>
2019-11-26 20:05:23 +0200

    create separate list of upper keys to remove from dict

--- jira/utils/__init__.py
+++ jira/utils/__init__.py
@@ -36,18 +36,14 @@ class CaseInsensitiveDict(dict):
     def __init__(self, *args, **kw):
         super(CaseInsensitiveDict, self).__init__(*args, **kw)
 
-#        self.itemlist = {}
-#        for key, value in super(CaseInsensitiveDict, self).items():
-#            if key != key.lower():
-#                self[key.lower()] = value
-#                self.pop(key, None)
+        upper_keys_list = []
+        for key in super(CaseInsensitiveDict, self).keys():
+            if key != key.lower():
+                upper_keys_list.append(key)
 
-        # self.itemlist[key.lower()] = value
-        itemlist = {}
-        for key, value in super(CaseInsensitiveDict, self).items():
-            itemlist[key.lower()] = value
-        self = itemlist
-
+        for upper_key in upper_keys_list:
+            self[upper_key.lower()] = self[upper_key]
+            self.pop(upper_key, None)
 
     def __setitem__(self, key, value):
         """Overwrite [] implementation."""
