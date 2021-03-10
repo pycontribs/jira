613458674290b5f81bad5d047a56d2ba2d2e5465
Chris S <truedoom@gmail.com>
2021-03-10 17:42:01 +0000

    Update client.py
    Chance made to solve error:
    
        c.a.p.r.c.security.jersey.XsrfResourceFilter] Use of the 'nocheck' value for X-Atlassian-Token has been deprecated since rest 3.0.0. Please use a value of 'no-check' instead.

--- jira/client.py
+++ jira/client.py
@@ -883,7 +883,7 @@ class JIRA(object):
                 url,
                 files={"file": (fname, attachment, "application/octet-stream")},
                 headers=CaseInsensitiveDict(
-                    {"content-type": None, "X-Atlassian-Token": "nocheck"}
+                    {"content-type": None, "X-Atlassian-Token": "no-check"}
                 ),
             )
         else:
@@ -903,7 +903,7 @@ class JIRA(object):
                 url,
                 data=m,
                 headers=CaseInsensitiveDict(
-                    {"content-type": m.content_type, "X-Atlassian-Token": "nocheck"}
+                    {"content-type": m.content_type, "X-Atlassian-Token": "no-check"}
                 ),
                 retry_data=file_stream,
             )
