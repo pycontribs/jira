16c105302623489992b4e302f247dc73026df787
studioj <22102283+studioj@users.noreply.github.com>
2021-05-15 14:10:01 +0200

    Fix docstring generation of jira client and minor doc layouting fix (#1038)
    * some part of the documentation wasn't properly generated.
    see #1035
    
    * adding warnings about deprecated auth mechanism for jira cloud in examples
    
    * this should fix the mismatch between the white background and the line length of the docs
    
    Co-authored-by: Neefs, Jef <jef.neefs@barco.com>

--- docs/conf.py
+++ docs/conf.py
@@ -167,7 +167,7 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {"body_max_width": "100%"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -193,6 +193,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_style = "css/custom_width.css"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
