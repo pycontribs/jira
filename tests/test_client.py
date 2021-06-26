3195c83015a0c5c243d892a71606d8ef91863703
adehad <26027314+adehad@users.noreply.github.com>
2021-06-26 17:00:43 +0100

    add tests for unclobbered headers update
    asda

--- tests/test_client.py
+++ tests/test_client.py
@@ -55,6 +55,16 @@ def slug(request, cl_admin):
     return slug
 
 
+@pytest.fixture()
+def no_fields(monkeypatch):
+    """When we want to test the __init__ method of the jira.client.JIRA
+    we don't need any external calls to get the fields.
+
+    We don't need the features of a MagicMock, hence we don't use it here.
+    """
+    monkeypatch.setattr(jira.client.JIRA, "fields", lambda *args, **kwargs: [])
+
+
 def test_delete_project(cl_admin, cl_normal, slug):
 
     assert cl_admin.delete_project(slug)
@@ -119,3 +129,53 @@ def test_result_list_if_empty():
 
     with pytest.raises(StopIteration):
         next(results)
+
+
+@pytest.mark.parametrize(
+    "options_arg",
+    [
+        {"headers": {"Content-Type": "application/json;charset=UTF-8"}},
+        {"headers": {"random-header": "nice random"}},
+        {},
+    ],
+    ids=["overwrite", "new", "none"],
+)
+def test_headers_unclobbered_update(options_arg, no_fields):
+
+    # GIVEN: the headers and the expected value
+    header_to_check: str
+    expected_header_value: str
+    if options_arg:
+        header_to_check = list(options_arg["headers"].keys())[0]
+        expected_header_value = options_arg["headers"][header_to_check]
+    elif "headers" not in options_arg:
+        # when we are checking not proving any headers
+        header_to_check = "Content-Type"
+        expected_header_value = jira.client.JIRA.DEFAULT_OPTIONS["headers"][
+            header_to_check
+        ]
+
+    invariant_header_name: str = "X-Atlassian-Token"
+    invariant_header_value: str = jira.client.JIRA.DEFAULT_OPTIONS["headers"][
+        invariant_header_name
+    ]
+    if options_arg:
+        # We arbitrarily chose a header to check it remains unchanged/unclobbered
+        # so should not be overwritten by a test case
+        assert (
+            invariant_header_name not in options_arg["headers"]
+        ), f"{invariant_header_name} is checked as not being overwritten in this test"
+
+    # WHEN: we initialise the JIRA class and get the headers
+    jira_client = jira.client.JIRA(
+        server="https://jira.atlasian.com",
+        get_server_info=False,
+        validate=False,
+        options=options_arg,
+    )
+
+    session_headers = jira_client._session.headers
+
+    # THEN: we have set the right headers and not affect the defaults
+    assert session_headers[header_to_check] == expected_header_value
+    assert session_headers[invariant_header_name] == invariant_header_value
