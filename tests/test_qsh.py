3466c306014a3d5dc072043b4d8303ae9f51d50e
Robin Hughes <robinhughes@fastmail.fm>
2021-09-23 10:58:31 +0100

    - Fix QshGenerator to combine repeated query string parameters into csv - add test for QshGenerator.generate_qsh

---
+++ tests/test_qsh.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from jira.client import QshGenerator
+
+
+class MockRequest(object):
+    def __init__(self, method, url):
+        self.method = method
+        self.url = url
+
+
+def test_qsh():
+    gen = QshGenerator("http://example.com")
+
+    for method, url, expected in [
+        ("GET", "http://example.com", "GET&&"),
+        # empty parameter
+        ("GET", "http://example.com?key=&key2=A", "GET&&key=&key2=A"),
+        # whitespace
+        ("GET", "http://example.com?key=A+B", "GET&&key=A%20B"),
+        # tilde
+        ("GET", "http://example.com?key=A~B", "GET&&key=A~B"),
+        # repeated parameters
+        (
+            "GET",
+            "http://example.com?key2=Z&key1=X&key3=Y&key1=A",
+            "GET&&key1=A,X&key2=Z&key3=Y",
+        ),
+        # repeated parameters with whitespace
+        (
+            "GET",
+            "http://example.com?key2=Z+A&key1=X+B&key3=Y&key1=A+B",
+            "GET&&key1=A%20B,X%20B&key2=Z%20A&key3=Y",
+        ),
+    ]:
+
+        req = MockRequest(method, url)
+        assert gen._generate_qsh(req) == expected
