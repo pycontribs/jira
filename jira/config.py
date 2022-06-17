1451288bb4ebc2e5f80b5dd902d1f8cd0357a1be
Markus Henn <brezelman@users.noreply.github.com>
2022-06-17 11:29:20 +0200

    don't edit JIRA.DEFAULT_OPTIONS
    instead work with shallow copy

--- jira/config.py
+++ jira/config.py
@@ -5,6 +5,7 @@ Also, this simplifies the scripts by not having to write the same initialization
 
 """
 import configparser
+import copy
 import logging
 import os
 import sys
@@ -120,7 +121,7 @@ def get_jira(
                 % __name__
             )
 
-    options = JIRA.DEFAULT_OPTIONS
+    options = copy.copy(JIRA.DEFAULT_OPTIONS)
     options["server"] = url
     options["autofix"] = autofix
     options["appid"] = appid
