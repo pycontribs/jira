9c229da2d92e6b18833a8ada19ae418a31633153
FatCat Wang <mr.jiadewang@gmail.com>
2022-11-30 16:35:10 +0800

    fix(docstring): 'remote lint' to 'remote link'

--- jira/client.py
+++ jira/client.py
@@ -1978,7 +1978,7 @@ class JIRA:
             relationship (Optional[str]): relationship description for the link (see the above link for details)
 
         Returns:
-            RemoteLink: the added remote lint
+            RemoteLink: the added remote link
         """
         try:
             applicationlinks: List[Dict] = self.applicationlinks()
@@ -2058,7 +2058,7 @@ class JIRA:
             object (Dict[str,Any]): the dictionary used to create remotelink data
 
         Returns:
-            RemoteLint
+            RemoteLink
         """
         data = {"object": object}
         url = self._get_url("issue/" + str(issue) + "/remotelink")
