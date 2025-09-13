6dfb4a98c9b7d06e1d05585a481143e1644036d2
studioj <22102283+studioj@users.noreply.github.com>
2025-09-13 18:04:13 +0200

    test: add `@allow_on_cloud` decorator to `test_custom_field_option` in CustomFieldOptionTests

--- tests/resources/test_custom_field_option.py
+++ tests/resources/test_custom_field_option.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-from tests.conftest import JiraTestCase
+from tests.conftest import JiraTestCase, allow_on_cloud
 
 
 class CustomFieldOptionTests(JiraTestCase):
+    @allow_on_cloud
     def test_custom_field_option(self):
+        expected = "Extensive / Widespread"
+        if not self.jira._is_cloud:
+            expected = "To Do"
         option = self.jira.custom_field_option("10000")
-        self.assertEqual(option.value, "To Do")
+        self.assertEqual(option.value, expected)
